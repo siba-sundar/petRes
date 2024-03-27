3026f68d63a4cc4f0dc9eb7a83b73bdbf9ad4953
Siba Sundar <siba.edu2003@gmail.com>
2024-03-27 22:48:07 +0530

    login page  complete

--- frontend/src/components/loginSignup/login.jsx
+++ frontend/src/components/loginSignup/login.jsx
@@ -1,3 +1,4 @@
+import loginImg from "../assets/loginImage.svg"
 import "./login.css"
 
 function LoginPage (){
@@ -5,8 +6,21 @@ function LoginPage (){
         <>
         <div className="loginMainContainer">
             <div className="loginContentContainer">
-                <div className="loginImage"></div>
-                <div className="loginData"></div>
+                <div className="loginImage">
+                    <img src={loginImg} alt="" />
+                </div>
+                <div className="loginData">
+                    <h2>LogIn To Your Account</h2>
+                    <label htmlFor="loginName">Username</label>
+                    <input type="text" className="loginName" />
+                    <label htmlFor="loginPass">Password</label>
+                    <input type="password" className="loginPass" />
+                    <button className="loginLoginButton">Login</button>
+                    <div className="signInOption">
+                        <p>Don't have an Account?</p>
+                        <a href="#">Sign In</a>
+                    </div>
+                </div>
             </div>
         </div>
         </>
