3cf4367f3c85f6c71448a8436506bbec779464e1
Siba Sundar <siba.edu2003@gmail.com>
2024-05-04 13:01:10 +0530

    adoption card

---
+++ frontend/src/components/adoption/adoptionCard.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import "./adoptionCard.css"
+import placeholderImage from '../assets/placeholder.svg'; // Import placeholder image
+
+const RandomPets = () => {
+  const [randomPets, setRandomPets] = useState([]);
+
+  useEffect(() => {
+    async function fetchRandomPets() {
+      try {
+        const response = await axios.get('http://localhost:4001/api/pets/all');
+        // Shuffle the array to get random pets
+        const shuffledPets = response.data.sort(() => 0.5 - Math.random());
+        // Get the first 3 elements (random)
+        const randomThreePets = shuffledPets.slice(0, 3);
+        setRandomPets(randomThreePets);
+      } catch (error) {
+        console.error('Error fetching random pets:', error);
+      }
+    }
+    fetchRandomPets();
+  }, []);
+
+  return (
+    <div>
+        <div className="adopMainContainer">
+        {randomPets.map((value, index) => (
+            <div className="adopPetCard" key={index}>
+                <div className="adopImg">
+                    {/* Use placeholder image if image URL is not available */}
+                    <img src={"http://localhost:4001/" + value.image || placeholderImage} alt="Pet" />
+                </div>
+                <div className="petDetails">
+                    <p>Name: {value.name}</p>
+                    <p>Breed: {value.breed}</p>
+                    <p>{value.description}</p>
+                </div>
+            </div>
+            ))}
+        </div>
+    </div>
+  );
+};
+
+export default RandomPets;
